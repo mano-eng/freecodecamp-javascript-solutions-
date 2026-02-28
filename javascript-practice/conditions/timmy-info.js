e3bd2c9078141e22ae0055a4a443f59b02fb78ed
mano-eng <mahmoud.ahmed65@msa.edu.eg>
2026-03-01 00:41:33 +0200

    Add Timmy's employment and gaming status checks

---
+++ javascript-practice/conditions/timmy-info.js
@@ -0,0 +1,20 @@
+ // This file contains simple practice with
+const hasDeveloperJob = true;
+
+if (hasDeveloperJob) {
+  console.log("Timmy is employed as a developer.");
+}
+
+const isTimmyAGamer = false;
+
+if (isTimmyAGamer) {
+  console.log("Timmy loves to play World of Warcraft.");
+}
+
+const timmyAge = 18;
+
+if (timmyAge >= 16) {
+  console.log("Timmy is old enough to drive.");
+} else {
+  console.log("Timmy is not old enough to drive.");
+}
